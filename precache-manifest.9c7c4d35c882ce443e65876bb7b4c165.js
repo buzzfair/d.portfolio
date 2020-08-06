23710036c751826a1a4a2f434477e1921ff2352a
buzzfair <guin.white@gmail.com>
2020-08-06 17:17:05 -0600

    Updates

---
+++ precache-manifest.9c7c4d35c882ce443e65876bb7b4c165.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ae5a782b1c72e01cbf2712057236e3",
+    "url": "/buzzfair/d.portfolio.git/index.html"
+  },
+  {
+    "revision": "433cfd489b79649edc0d",
+    "url": "/buzzfair/d.portfolio.git/static/css/2.4eb38d7c.chunk.css"
+  },
+  {
+    "revision": "4c249b3dd3e546e9d25c",
+    "url": "/buzzfair/d.portfolio.git/static/css/main.6e515215.chunk.css"
+  },
+  {
+    "revision": "433cfd489b79649edc0d",
+    "url": "/buzzfair/d.portfolio.git/static/js/2.9bc6e910.chunk.js"
+  },
+  {
+    "revision": "cbf3f692f559dbbebbdddcdb5e937bd4",
+    "url": "/buzzfair/d.portfolio.git/static/js/2.9bc6e910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c249b3dd3e546e9d25c",
+    "url": "/buzzfair/d.portfolio.git/static/js/main.2053ae4d.chunk.js"
+  },
+  {
+    "revision": "357541adeec7da5661e4",
+    "url": "/buzzfair/d.portfolio.git/static/js/runtime-main.3e308934.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/buzzfair/d.portfolio.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/buzzfair/d.portfolio.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/buzzfair/d.portfolio.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/buzzfair/d.portfolio.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
